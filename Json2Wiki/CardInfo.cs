19293e3761e2769338d51024ed683bb550bda927
Ellis <ellisnewmanem@outlook.com>
2018-09-16 21:14:54 +0800

    J2W update & bug fix

---
+++ Json2Wiki/CardInfo.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Json2Wiki
+{
+    public class Costume
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// [あじさいドロップ]春日未来
+        /// </summary>
+        public string name { get; set; }
+
+        public string description { get; set; }
+    }
+
+    public class BonusCostume
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// [あじさいドロップ+]春日未来
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 「あじさいドロップ」の
+        /// </summary>
+        public string description { get; set; }
+    }
+
+    public class CenterEffect
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// Princessタイプのみ編成時、ボーカル値が95％アップ
+        /// </summary>
+        public string description { get; set; }
+        /// <summary>
+        /// IdolType
+        /// </summary>
+        public int idolType { get; set; }
+        /// <summary>
+        /// SpecificIdolType
+        /// </summary>
+        public int specificIdolType { get; set; }
+        /// <summary>
+        /// Attribute
+        /// </summary>
+        public int attribute { get; set; }
+        /// <summary>
+        /// Value
+        /// </summary>
+        public int value { get; set; }
+    }
+
+    public class Skill
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 9秒ごとに{0}％の確率で5秒間、Perfectのスコアが10％アップ、コンボボーナスが5％アップ
+        /// </summary>
+        public string description { get; set; }
+        /// <summary>
+        /// EffectId
+        /// </summary>
+        public int effectId { get; set; }
+        /// <summary>
+        /// Evaluation
+        /// </summary>
+        public int evaluation { get; set; }
+        /// <summary>
+        /// Duration
+        /// </summary>
+        public int duration { get; set; }
+        /// <summary>
+        /// Interval
+        /// </summary>
+        public int interval { get; set; }
+        /// <summary>
+        /// Probability
+        /// </summary>
+        public int probability { get; set; }
+        /// <summary>
+        /// Value
+        /// </summary>
+        public List<int> value { get; set; }
+    }
+
+    public class CardInfo
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 無邪気なレイニーデイズ　春日未来
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// SortId
+        /// </summary>
+        public int sortId { get; set; }
+        /// <summary>
+        /// IdolId
+        /// </summary>
+        public int idolId { get; set; }
+        /// <summary>
+        /// 014mir0064
+        /// </summary>
+        public string resourceId { get; set; }
+        /// <summary>
+        /// Rarity
+        /// </summary>
+        public int rarity { get; set; }
+        /// <summary>
+        /// ExtraType
+        /// </summary>
+        public int extraType { get; set; }
+        /// <summary>
+        /// Costume
+        /// </summary>
+        public Costume costume { get; set; }
+        /// <summary>
+        /// BonusCostume
+        /// </summary>
+        public BonusCostume bonusCostume { get; set; }
+
+        public string flavorText { get; set; }
+
+        public string flavorTextAwakened { get; set; }
+        /// <summary>
+        /// LevelMax
+        /// </summary>
+        public int levelMax { get; set; }
+        /// <summary>
+        /// LevelMaxAwakened
+        /// </summary>
+        public int levelMaxAwakened { get; set; }
+        /// <summary>
+        /// VocalMin
+        /// </summary>
+        public int vocalMin { get; set; }
+        /// <summary>
+        /// VocalMax
+        /// </summary>
+        public int vocalMax { get; set; }
+        /// <summary>
+        /// VocalMinAwakened
+        /// </summary>
+        public int vocalMinAwakened { get; set; }
+        /// <summary>
+        /// VocalMaxAwakened
+        /// </summary>
+        public int vocalMaxAwakened { get; set; }
+        /// <summary>
+        /// VocalMasterBonus
+        /// </summary>
+        public int vocalMasterBonus { get; set; }
+        /// <summary>
+        /// DanceMin
+        /// </summary>
+        public int danceMin { get; set; }
+        /// <summary>
+        /// DanceMax
+        /// </summary>
+        public int danceMax { get; set; }
+        /// <summary>
+        /// DanceMinAwakened
+        /// </summary>
+        public int danceMinAwakened { get; set; }
+        /// <summary>
+        /// DanceMaxAwakened
+        /// </summary>
+        public int danceMaxAwakened { get; set; }
+        /// <summary>
+        /// DanceMasterBonus
+        /// </summary>
+        public int danceMasterBonus { get; set; }
+        /// <summary>
+        /// VisualMin
+        /// </summary>
+        public int visualMin { get; set; }
+        /// <summary>
+        /// VisualMax
+        /// </summary>
+        public int visualMax { get; set; }
+        /// <summary>
+        /// VisualMinAwakened
+        /// </summary>
+        public int visualMinAwakened { get; set; }
+        /// <summary>
+        /// VisualMaxAwakened
+        /// </summary>
+        public int visualMaxAwakened { get; set; }
+        /// <summary>
+        /// VisualMasterBonus
+        /// </summary>
+        public int visualMasterBonus { get; set; }
+        /// <summary>
+        /// Life
+        /// </summary>
+        public int life { get; set; }
+        /// <summary>
+        /// CenterEffect
+        /// </summary>
+        public CenterEffect centerEffect { get; set; }
+        /// <summary>
+        /// Princessディーヴァ
+        /// </summary>
+        public string centerEffectName { get; set; }
+        /// <summary>
+        /// Skill
+        /// </summary>
+        public List<Skill> skill { get; set; }
+        /// <summary>
+        /// 愛らしく咲いて
+        /// </summary>
+        public string skillName { get; set; }
+        /// <summary>
+        /// 2018-06-29T15:00:00+09:00
+        /// </summary>
+        public string addDate { get; set; }
+    }
+
+    public class Root
+    {
+        /// <summary>
+        /// CardInfo2
+        /// </summary>
+        public CardInfo CardInfo { get; set; }
+        /// <summary>
+        /// TrimArray
+        /// </summary>
+        public string trimArray { get; set; }
+        /// <summary>
+        /// Remove
+        /// </summary>
+        public string remove { get; set; }
+        /// <summary>
+        /// Unique
+        /// </summary>
+        public string unique { get; set; }
+        /// <summary>
+        /// SameCount
+        /// </summary>
+        public string sameCount { get; set; }
+        /// <summary>
+        /// Contains
+        /// </summary>
+        public string contains { get; set; }
+    }
+}
--- Json2Wiki/Program.cs
+++ Json2Wiki/Program.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿//-------------------------------------------
+//by Excel
+//
+//ver alpha 0.35
+//
+//-------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Web.Script.Serialization;
 
-namespace testJsonAPI
+namespace Json2Wiki
 {
     class Program
     {
@@ -24,18 +31,27 @@ namespace testJsonAPI
                 }
 
                 string result = GetWebRequest("http" + "s://a" + "pi.mat" + "suri" + "hi.me/ml" + "td/v" + "1/c" + "ards/" + input);
-
+                result = System.Text.RegularExpressions.Regex.Replace(result, "[\r\n\t]", "");
+                
                 if (result == "[]")
                 {
-                    Console.WriteLine("查询错误");
+                    Console.WriteLine("ERROR! 查询错误");
+                    Console.WriteLine();
+                    continue;
                 }
-                else
+
+                //List<CardInfo> infoList = JsonConvert.DeserializeObject<List<CardInfo>>(result);
+
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                List<CardInfo> infoList = js.Deserialize<List<CardInfo>>(result);
+
+                foreach (var info in infoList)
                 {
-                    result = GetString(GetCardInfo(result));
+                    result = GetString(info);
                     Console.WriteLine();
                     Console.WriteLine(result);
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
             }
         }
 
@@ -46,12 +62,13 @@ namespace testJsonAPI
             WebResponse result = myReq.GetResponse();
             Stream receviceStream = result.GetResponseStream();
             StreamReader readerOfStream = new StreamReader(receviceStream, Encoding.GetEncoding("utf-8"));
-            
+
             string strHTML = readerOfStream.ReadToEnd();
 
             readerOfStream.Close();
             receviceStream.Close();
             result.Close();
+
             return strHTML;
         }
 
@@ -59,222 +76,25 @@ namespace testJsonAPI
         {
             string origin =
                 "{{CardData" +
-                " | CardName = " + info.cardName +
+                " | CardName = " + info.name +
                 " | CardNameCN = " +
-                " | Rarity = " + info.rarity +
-                " | Type = " + info.idolType +
-                " | CharaName = " + info.charaName +
-                " | Li1 = " + info.life + " | Vo1 = " + info.vo1 + " | Da1 = " + info.da1 + " | Vi1 = " + info.vi1 +
-                " | Li2 = " + info.life + " | Vo2 = " + info.vo2 + " | Da2 = " + info.da2 + " | Vi2 = " + info.vi2 +
-                " | Li3 = " + info.life + " | Vo3 = " + info.vo3 + " | Da3 = " + info.da3 + " | Vi3 = " + info.vi3 +
-                " | LeaderSkillName = " + info.leaderSkillName +
+                " | Rarity = " + Define.idolRarity[info.rarity] +
+                " | Type = " + Define.idolType[info.idolId] +
+                " | CharaName = " + Define.idolName[info.idolId] +
+                " | Li1 = " + info.life + " | Vo1 = " + info.vocalMax + " | Da1 = " + info.danceMax + " | Vi1 = " + info.visualMax +
+                " | Li2 = " + info.life + " | Vo2 = " + info.vocalMaxAwakened + " | Da2 = " + info.danceMaxAwakened + " | Vi2 = " + info.visualMaxAwakened +
+                " | Li3 = " + info.life + " | Vo3 = " + (info.vocalMasterBonus * 4 + info.vocalMaxAwakened) + " | Da3 = " + (info.danceMasterBonus * 4 + info.danceMaxAwakened) + " | Vi3 = " + (info.visualMasterBonus * 4 + info.visualMaxAwakened) +
+                " | LeaderSkillName = " + info.centerEffectName +
                 " | LeaderSkillNameCN = " +
-                " | LeaderSkill = " + info.leaderSkill +
+                " | LeaderSkill = " + (info.centerEffect == null ? "" : info.centerEffect.description) +
                 " | LeaderSkillCN = " +
-                " | CardSkillName = " + info.cardSkillName +
+                " | CardSkillName = " + info.skillName +
                 " | CardSkillNameCN = " +
-                " | CardSkill = " + info.cardSkill +
+                " | CardSkill = " + (info.skill == null ? "" : info.skill[0].description) +
                 " | CardSkillCN = " +
                 " | }}";
 
             return origin;
         }
-
-
-
-        static CardInfo GetCardInfo(string input)
-        {
-            CardInfo info = new CardInfo();
-
-            string[] strings;
-            strings = input.Split(',');
-
-            for (int i = 0; i < strings.Length; i++)
-            {
-                string str = strings[i];
-                str = System.Text.RegularExpressions.Regex.Replace(str, "[\r\n\t]", "");
-                str = str.Replace(" ", "");
-
-                string[] strs = str.Split(':');
-                string key = strs[0];
-
-                if (key.Contains("\""))
-                    key = key.Split('"')[1];
-
-
-                string value = str;
-                
-                if(strs.Length > 1)
-                    value = strs[1];
-
-                if (key == "name" && string.IsNullOrEmpty(info.cardName))
-                    info.cardName = value.Replace("\"","");
-
-                if (key == "idolId")
-                {
-                    info.charaName = Define.idolName[value];
-                    info.idolType = Define.idolType[value];
-                }
-
-                if (key == "rarity")
-                    switch (value)
-                    {
-                        case "1":
-                            info.rarity = "N";
-                            break;
-                        case "2":
-                            info.rarity = "R";
-                            break;
-                        case "3":
-                            info.rarity = "SR";
-                            break;
-                        case "4":
-                            info.rarity = "SSR";
-                            break;
-                    }
-
-
-                if (key == "centerEffectName")
-                    info.leaderSkillName = value.Replace("\"", "");
-
-                if (key == "description" && string.IsNullOrEmpty(info.leaderSkillName))
-                    info.leaderSkill = value.Replace("\"", "");
-
-                else if (key == "description") info.cardSkill = value.Replace("\"", "");
-
-                if (key == "skillName")
-                    info.cardSkillName = value.Replace("\"", "");
-
-                if (key == "life")
-                    info.life = value;
-
-                if (key == "visualMax")
-                    info.vi1 = value;
-
-                if (key == "visualMaxAwakened")
-                    info.vi2 = value;
-
-                if (key == "visualMasterBonus")
-                    info.vi3 = (Int32.Parse(value) * 4 + Int32.Parse(info.vi2)).ToString();
-
-                if (key == "vocalMax")
-                    info.vo1 = value;
-
-                if (key == "vocalMaxAwakened")
-                    info.vo2 = value;
-
-                if (key == "vocalMasterBonus")
-                    info.vo3 = (Int32.Parse(value) * 4 + Int32.Parse(info.vo2)).ToString();
-
-                if (key == "danceMax")
-                    info.da1 = value;
-
-                if (key == "danceMaxAwakened")
-                    info.da2 = value;
-
-                if (key == "danceMasterBonus")
-                    info.da3 = (Int32.Parse(value) * 4 + Int32.Parse(info.da2)).ToString();
-
- 
-            }
-
-
-            return info;
-        }
     }
-
-    class CardInfo
-    {
-        public string cardName;
-        public string rarity;
-        public string idolType;
-        public string charaName;
-        public string vi1;
-        public string vi2;
-        public string vi3;
-        public string da1;
-        public string da2;
-        public string da3;
-        public string vo1;
-        public string vo2;
-        public string vo3;
-        public string life;
-        public string leaderSkill;
-        public string leaderSkillName;
-        public string cardSkill;
-        public string cardSkillName;
-    }
-
-    /*
-    class CardInfo
-    {
-        public int id;
-        public string cardName;
-        public int sortId;
-        public int idolId;
-        public string resourceId;
-        public int rarity;
-        public int extraType;
-        public Costume costume;
-        public BonusCostume bonusCostume;
-        public string flavorText;
-        public string flavorTextAwakened;
-        public int levelMax;
-        public int levelMaxAwakened;
-        public int vocalMin;
-        public int vocalMax;
-        public int vocalMinAwakened;
-        public int vocalMaxAwakened;
-        public int vocalMasterBonus;
-        public int danceMin;
-        public int danceMinAwakened;
-        public int danceMaxAwakened;
-        public int danceMasterBonus;
-        public int visualMin;
-        public int visualMax;
-        public int visualMinAwakened;
-        public int visualMaxAwakened;
-        public int visualMasterBonus;
-        public int life;
-        public CenterEffect centerEffect;
-        public string centerEffectName;
-        public Skill[] skill;
-        public string skillName;
-        public string addDate;
-    }
-
-    class Costume
-    {
-        public long id;
-        public string cardName;
-        public string description;
-    }
-
-    class BonusCostume
-    {
-        public long id;
-        public string cardName;
-        public string description;
-    }
-
-    class CenterEffect
-    {
-        public int id;
-        public string description;
-        public int idolType;
-        public int attribute;
-        public int value;
-    }
-
-    class Skill
-    {
-        public int id;
-        public string description;
-        public int effectId;
-        public int evaluation;
-        public int duration;
-        public int interval;
-        public int probability;
-        public int[] value;
-    }*/
 }
